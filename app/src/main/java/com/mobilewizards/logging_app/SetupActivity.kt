8bfefdd735ccfdbfe8cbbbaf2274ac793a961fb3
Juho Naatula <juho.naatula@tuni.fi>
2024-12-08 18:11:46 +0200

    deleting duplicate activity file

--- app/src/main/java/com/mobilewizards/logging_app/SetupActivity.kt
+++ app/src/main/java/com/mobilewizards/logging_app/SetupActivity.kt
@@ -5,15 +5,15 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
-class SetupActivity : AppCompatActivity() {
+class SetupActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup)
 
         val confirmButton = findViewById<Button>(R.id.confirmButton)
-        confirmButton.setOnClickListener{
-            val mainIntent = Intent(this@SetupActivity, SettingsActivityDuplicated::class.java).apply {
+        confirmButton.setOnClickListener {
+            val mainIntent = Intent(this@SetupActivity, PhoneSettingPage::class.java).apply {
                 flags = Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
             }
             startActivity(mainIntent)
